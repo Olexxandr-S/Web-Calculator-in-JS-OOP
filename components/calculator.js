7d29df0c9173bb91140d3dd5c5decdac116df4af
OlexxandrS <94759574+OlexxandrS@users.noreply.github.com>
2022-08-26 14:08:13 +0300

    add_all-history

--- components/calculator.js
+++ components/calculator.js
@@ -1,67 +1,69 @@
-import Screen from './screen.js';
-import Keyboard from './keyboard.js';
-import CalculationStack from './CalculationStack.js';
-import Action from './Action.js';
+import Screen from "./screen.js";
+import Keyboard from "./keyboard.js";
+import CalculationStack from "./CalculationStack.js";
+import Action from "./Action.js";
 
-export default class Calcualator {
-  static root = document.querySelector('#root');
+export default class Calculator {
+  static root = document.querySelector("#root");
   static screen = new Screen();
   static keyboard = new Keyboard();
 
   static stack = new CalculationStack();
 
   static createCalculator() {
-    Calcualator.root.appendChild(Calcualator.screen.getView());
-    Calcualator.root.appendChild(Calcualator.keyboard.getView());
+    Calculator.root.appendChild(Calculator.screen.getView());
+    Calculator.root.appendChild(Calculator.keyboard.getView());
   }
 
-  static keyPressed({event, name, symbol}) {
-    if((/^[0-9]$/ig).test(symbol)) {
-      
-      let newNumber = Calcualator.stack.addDigit(symbol);
-      Calcualator.screen.display.display(newNumber);
-
-    } else if(["/", "*", "+", "-"].includes(symbol)) {
-      
-      if (Calcualator.stack.a && Calcualator.stack.b && Calcualator.stack.action) {
-        let result = Calcualator.stack.calculate(); 
-        Calcualator.screen.display.display(result);
-        Calcualator.stack.a = result;
-        Calcualator.stack.b = null;
+  static keyPressed({ event, name, symbol }) {
+    if (/^[0-9]$/gi.test(symbol)) {
+      let newNumber = Calculator.stack.addDigit(symbol);
+      Calculator.screen.display.display(newNumber);
+    } else if (["/", "*", "+", "-"].includes(symbol)) {
+      if (Calculator.stack.a && Calculator.stack.b && Calculator.stack.action) {
+        let result = Calculator.stack.calculate();
+        Calculator.screen.display.display(result);
+        Calculator.stack.a = result;
+        Calculator.stack.b = null;
       }
 
       const action = new Action(symbol);
-      Calcualator.stack.setAction(action);
-      Calcualator.screen.history.add({a: Calcualator.stack.a, action: Calcualator.stack.action.symbol});
-      Calcualator.screen.history.render();
-
-    } else if(symbol === "=") {
-      
-      if (Calcualator.stack.b && Calcualator.stack.action) {
-        Calcualator.screen.history.add({a: Calcualator.stack.a, action: Calcualator.stack.action.symbol});
-
-      } else if (Calcualator.stack.a && Calcualator.stack.action) {
-        Calcualator.stack.b = Calcualator.stack.a;
+      Calculator.stack.setAction(action);
+      Calculator.screen.history.add({
+        a: Calculator.stack.a,
+        action: Calculator.stack.action.symbol,
+      });
+      Calculator.screen.history.render();
+    } else if (symbol === "=") {
+      if (Calculator.stack.b && Calculator.stack.action) {
+        Calculator.screen.history.add({
+          a: Calculator.stack.a,
+          action: Calculator.stack.action.symbol,
+        });
+      } else if (Calculator.stack.a && Calculator.stack.action) {
+        Calculator.stack.b = Calculator.stack.a;
       }
-      
-      let result = Calcualator.stack.calculate();
-      
-      Calcualator.screen.display.display(result);
-      Calcualator.screen.history.update({b: Calcualator.stack.b, result: result});
-      Calcualator.screen.history.render();
-      Calcualator.stack.a = result;
 
-    } else if(symbol === ".") {
-      Calcualator.stack.dp = true;
+      let result = Calculator.stack.calculate();
 
-    } else if(name === "delete") {
-      Calcualator.stack.deleteLastDigit();
-      Calcualator.screen.display.display(Calcualator.stack.a);
-
-    } else if(name === "clear") {
-      Calcualator.stack.clear();
-      Calcualator.screen.display.display(Calcualator.stack.a);
-      Calcualator.screen.history.clear();
+      Calculator.screen.display.display(result);
+      Calculator.screen.history.update({
+        b: Calculator.stack.b,
+        result: result,
+      });
+      Calculator.screen.history.render();
+      Calculator.stack.a = result;
+    } else if (symbol === ".") {
+      Calculator.stack.dp = true;
+    } else if (name === "delete") {
+      Calculator.stack.deleteLastDigit();
+      !Calculator.stack.action
+        ? Calculator.screen.display.display(Calculator.stack.a)
+        : Calculator.screen.display.display(Calculator.stack.b);
+    } else if (name === "clear") {
+      Calculator.stack.clear();
+      Calculator.screen.display.display(Calculator.stack.a);
+      Calculator.screen.history.clear();
     }
   }
 }
